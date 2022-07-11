8b3b58726b64785da2cd152118cb46e492a45be9
Chad Stabler <stablerpsn@gmail.com>
2022-07-11 16:36:28 -0700

    more more blegh

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders p2', () => {
   render(<App />);
   const linkElement = screen.getByText(/player 2/i);
   expect(linkElement).toBeInTheDocument();
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders p2', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/player 2/i);
+  expect(linkElement).toBeInTheDocument();
+});
